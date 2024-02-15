3deeae19efc76226a101faa37783c2873075a607
PeterMattenet <pmattenet@gmail.com>
2024-02-15 18:57:09 -0300

    Why is bug?

--- Scripts/Test/Test.gd
+++ Scripts/Test/Test.gd
@@ -3,8 +3,14 @@ extends Node
 @export var starterDeck:BaseDeckData
 @export var playerCharacter:BaseCharacterData
 @export var testBattle:BaseBattleData
+@export var artifact:BaseArtifactData
 
 func _ready():
+	print("Test script. Checking artifact modifier conditionals")
+	print(artifact.id)
+	print(artifact.name)
+	print("Artifact has %s modifiers " % artifact.modifiers.size())
+	print(artifact.modifiers[0].modifier_conditionals.size())
 	GameManagerInstance.StartGame(
 		starterDeck,
 		12345,
